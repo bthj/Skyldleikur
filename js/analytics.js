e1fff1fb36e12227272aa05487ba1a3dc54580ed
Björn Þór Jónsson <bangsi@bthj.is>
2013-04-10 19:27:27 +0000

    Analytics

--- js/analytics.js
+++ js/analytics.js
@@ -4,6 +4,7 @@ document.addEventListener("unload", persistTasks, false);
 function onDeviceReady() {
     gaPlugin = window.plugins.gaPlugin;
     gaPlugin.init(successHandler, errorHandler, "UA-37626236-3", 10);
+    alert(gaPlugin);
 }
 
 function persistTasks() {
